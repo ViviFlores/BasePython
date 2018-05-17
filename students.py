37c3dfe536a88eb8d23bdcd44fef92cd92248362
ViviFlores <vivi_flores_1992@hotmail.com>
2018-05-17 11:49:33 -0500

    commit 'pruebafinal'

--- students.py
+++ students.py
@@ -9,6 +9,36 @@ class Student(Model):
     class Meta:
         database=db
 
+students=[
+    {'username':'Aldo',
+     'points':15},
+    {'username':'Betty',
+     'points':7},
+    {'username':'Ariel',
+     'points':10},
+    {'username':'Jairo',
+     'points':0},
+    {'username':'Dayana',
+     'points':9},
+]
+#metodo para añadir estudiantes
+def add_students():
+    for student in students:
+        try:
+            #crear mi regsitro
+            Student.create(username=student['username'],
+                           points=student['points'])
+        except IntegrityError: #cuando ya exista el registro
+            student_records= Student.get(username=student['username'])
+            student_records.points=student['points']
+            student_records.save()#guardar cambios
+#metodo que me obtenga el almuno con la calificacion mas alta
+def top_student():
+    topcalif= Student.select().order_by(Student.points.desc()).get()
+    return topcalif
+
 if __name__ == '__main__':
     db.connect()
     db.create_tables([Student],safe=True)
+    add_students()
+    print('El mejor estudiante es {}'.format(top_student().username))
